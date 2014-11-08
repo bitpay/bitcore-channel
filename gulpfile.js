d33e9df3e82505d60c16324fb1a5fec6204bb5bb
Esteban Ordano <eordano@gmail.com>
2014-11-07 21:41:52 -0300

    Make test fails for CI

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,20 @@ function ignoreError(err) {
   this.emit('end');
 }
 
-gulp.task('test', function() {
-  return gulp.src(tests).pipe(new gulp_mocha({reporter: 'spec'})).on('error', ignoreError);
+function testAllFiles() {
+  return gulp.src(tests).pipe(new gulp_mocha({reporter: 'spec'}));
+}
+
+gulp.task('test', testAllFiles);
+
+gulp.task('test-nofail', function() {
+  return testAllFiles().on('error', ignoreError);
 });
 
 gulp.task('watch:test', function() {
   // TODO: Only run tests that are linked to file changes by doing
   // something smart like reading through the require statements
-  return gulp.watch(alljs, ['test']);
+  return gulp.watch(alljs, ['test-nofail']);
 });
 
 gulp.task('jsdoc', function() {
@@ -35,7 +41,7 @@ gulp.task('jsdoc', function() {
 });
 
 gulp.task('lint', function() {
-  return gulp.src(alljs) 
+  return gulp.src(alljs)
     .pipe(gulp_jshint())
     .pipe(gulp_jshint.reporter('default'));
 });
