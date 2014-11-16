bce105cde3d379825a884a5b620afb9a59d7b209
Esteban Ordano <eordano@gmail.com>
2014-11-16 10:52:05 -0300

    refactoring structure

---
+++ test/Transactions.spec.js
@@ -0,0 +1,19 @@
+describe('Transactions: ', function() {
+
+
+  describe('Commitment', function() {
+
+    it('needs to be created from other inputs', function() {
+    });
+    it('needs two public keys to build the P2SH address', function() {
+    });
+    it('needs to be signed', function() {
+    });
+    it('validates that all inputs are signed', function() {
+    });
+    it('uses a multisig P2SH address', function() {
+    });
+    it('returns a valid hexa raw transaction', function() {
+    });
+  });
+});
