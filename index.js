4dc70663d438f4d225dac8518901e5d91b62128a
Esteban Ordano <eordano@gmail.com>
2015-01-18 11:28:19 -0300

    Export transactions

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ require('./lib/errors');
 
 module.exports = {
   Consumer: require('./lib/Consumer'),
-  Provider: require('./lib/Provider')
+  Provider: require('./lib/Provider'),
+  Transactions: {
+    Commitment: require('./lib/transactions/Commitment'),
+    Refund: require('./lib/transactions/Refund'),
+    Payment: require('./lib/transactions/Payment')
+  }
 };
