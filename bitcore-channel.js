dca243bce83fc8e6a766a5e3d27d649408153b73
Manuel Araoz <manuelaraoz@gmail.com>
2015-02-11 20:33:56 -0300

    Build: 0.10.0

---
+++ bitcore-channel.js
@@ -0,0 +1,492 @@
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var $ = require('bitcore').util.preconditions;
+
+var Script = require('bitcore').Script;
+var Networks = require('bitcore').Networks;
+var PrivateKey = require('bitcore').PrivateKey;
+var PublicKey = require('bitcore').PublicKey;
+var Address = require('bitcore').Address;
+var _ = require('bitcore').deps._;
+
+var Commitment = require('./transactions/commitment');
+var Payment = require('./transactions/payment');
+var Refund = require('./transactions/refund');
+
+var HOURS_IN_DAY = 24;
+var MINUTES_IN_HOUR = 60;
+var SECONDS_IN_MINUTE = 60;
+
+var ONE_DAY = SECONDS_IN_MINUTE * MINUTES_IN_HOUR * HOURS_IN_DAY;
+
+/**
+ * @param {Object} opts
+ * @param {string|Network} opts.network - 'livenet' or 'testnet'
+ * @param {number} opts.expires - unix timestamp in millis since epoch
+ *
+ * @param {bitcore.PrivateKey=} opts.commitmentKey - key to use when negotiating the channel
+ *
+ * @param {string=} opts.providerPublicKey - the public key for the server, in hexa compressed format
+ * @param {string=} opts.providerAddress - the final address where the server will be paid
+ *
+ * @param {bitcore.PrivateKey=} opts.fundingKey - key to use for funding the channel
+ * @param {string=} opts.refundAddress - address to use for refund/change
+ * @constructor
+ */
+function Consumer(opts) {
+  /*jshint maxstatements: 30*/
+  /*jshint maxcomplexity: 10*/
+  opts = opts || {};
+
+  /**
+   * @type {bitcore.Network}
+   * @desc Either 'livenet' or 'testnet'
+   */
+  this.network = Networks.get(opts.network || 'livenet');
+  /**
+   * @type number
+   * @desc The expiration date for the channel, in seconds since UNIX epoch
+   */
+  this.expires = opts.expires || Math.round(new Date().getTime() / 1000) + ONE_DAY;
+
+  /**
+   * @type bitcore.PrivateKey
+   * @desc This is the key used for the 2-of-2 locking of funds
+   */
+  this.commitmentKey = new PrivateKey(opts.commitmentKey);
+
+  /**
+   * @type {bitcore.PublicKey}
+   */
+  this.providerPublicKey = new PublicKey(opts.providerPublicKey);
+
+  /**
+   * @type {bitcore.Address|string}
+   * @desc The address where the server will be paid.
+   */
+  this.providerAddress = opts.providerAddress
+    ? new Address(opts.providerAddress)
+    : this.providerPublicKey.toAddress();
+
+  /**
+   * @type bitcore.PrivateKey
+   * @desc A private key for funding the channel. An alternative implementation could
+   * provide a list of unspent outputs and the keys needed to sign the outputs
+   */
+  this.fundingKey = opts.fundingKey || new PrivateKey();
+
+  /**
+   * @type bitcore.Address
+   * @desc The address where the user will pay to fund the channel
+   */
+  this.fundingAddress = this.fundingKey.toAddress();
+
+  if (opts.refundAddress) {
+    if (opts.refundAddress instanceof Address) {
+      /**
+       * @type bitcore.Address
+       * @desc The address where the refund will go to (also used for the change)
+       */
+      this.refundAddress = opts.refundAddress;
+    } else {
+      this.refundAddress = new Address(opts.refundAddress);
+    }
+  } else {
+    /**
+     * @type bitcore.PrivateKey
+     * @desc If no refund address is provided, a private key is generated for the user
+     */
+    this.refundKey = new PrivateKey();
+    this.refundAddress = new Address(this.refundKey.publicKey, this.network);
+  }
+
+  /**
+   * @name Consumer#commitmentTx
+   * @type Commitment
+   * @desc The commitment transaction for this channel
+   */
+  this.commitmentTx = new Commitment({
+    publicKeys: [this.commitmentKey.publicKey, this.providerPublicKey],
+    network: this.network
+  });
+}
+
+/**
+ * Adds an UTXO to the funding transaction. The funding transaction exists
+ * merely because we can't expect the wallet of the user to support payment
+ * channels.
+ *
+ * @param {Object} utxo
+ */
+Consumer.prototype.processFunding = function(utxo) {
+  $.checkArgument(_.isObject(utxo), 'Can only process an array of objects or an object');
+  this.commitmentTx.from(utxo);
+};
+
+/**
+ * Build the refund transaction (TX 2)
+ *
+ * @return {bitcore.Transaction}
+ */
+Consumer.prototype.setupRefund = function() {
+  this.commitmentTx.sign(this.fundingKey);
+  $.checkState(this.commitmentTx.isFullySigned());
+  var amount = this.commitmentTx.amount;
+  var multisigOut = {
+    txid: this.commitmentTx.id,
+    outputIndex: 0,
+    satoshis: amount,
+    script: this.commitmentTx.outputs[0].script
+  };
+  this.refundTx = new Refund()
+    .from(multisigOut, this.commitmentTx.publicKeys, 2)
+    .change(this.refundAddress);
+  this.refundTx.inputs[0].sequenceNumber = 0;
+  this.refundTx._updateChangeOutput();
+  this.refundTx.nLockTime = this.expires;
+  return this.refundTx;
+};
+
+/**
+ * Validates that a message contains a valid signature from the Provider
+ * that allows the Consumer to spend the lock transaction (TX 1)
+ *
+ * @param {string} messageFromProvider JSON-serialized message
+ * @return {boolean} true if the signature is valid
+ */
+Consumer.prototype.validateRefund = function(refund) {
+  refund = new Refund(refund);
+  refund.sign(this.commitmentKey);
+  $.checkState(new Address(refund.outputs[0].script, this.network).toString() ===
+               this.refundAddress.toString());
+  var amount = refund.outputs[0].satoshis;
+  $.checkState(amount + refund._estimateFee() === this.commitmentTx.amount);
+  $.checkState(refund.outputs.length === 1, 'More than expected outputs received');
+  $.checkState(refund.isFullySigned(), 'Refund was not fully signed');
+  $.checkState(Script.Interpreter().verify(
+    refund.inputs[0].script,
+    refund.inputs[0].output.script
+  ), 'Refund is incorrectly signed');
+  this.refundTx = refund;
+  var multisigOut = {
+    txid: this.commitmentTx.hash,
+    outputIndex: 0,
+    satoshis: amount,
+    script: this.commitmentTx.outputs[0].script
+  };
+  this.paymentTx = new Payment({
+    multisigOut: multisigOut,
+    amount: amount,
+    paymentAddress: this.providerAddress,
+    changeAddress: this.refundAddress,
+    publicKeys: this.commitmentTx.publicKeys,
+    network: this.network
+  });
+  this.paymentTx.sign(this.commitmentKey);
+  return true;
+};
+
+/**
+ * Increments the amount being paid by a given amount of satoshis.
+ * @return {bitcore.Transaction} the updated transaction
+ */
+Consumer.prototype.incrementPaymentBy = function (satoshis) {
+  this.paymentTx.updateValue(satoshis);
+  this.paymentTx.sign(this.commitmentKey);
+  return this.paymentTx.toObject();
+};
+
+/**
+ * Idiomatic shortcut to retrieve the payment transaction.
+ * @return {bitcore.Transaction}
+ */
+Consumer.prototype.getPaymentTx = function () {
+  return this.paymentTx;
+};
+
+module.exports = Consumer;
+
+},{"./transactions/commitment":4,"./transactions/payment":5,"./transactions/refund":6,"bitcore":"bitcore"}],2:[function(require,module,exports){
+'use strict';
+
+var spec = {
+  name: 'Channel',
+  message: 'Internal Error on bitcore-channels Module {0}',
+};
+
+module.exports = require('bitcore').errors.extend(spec);
+
+},{"bitcore":"bitcore"}],3:[function(require,module,exports){
+'use strict';
+
+var Payment = require('./transactions/payment');
+var Refund = require('./transactions/refund');
+
+var $ = require('bitcore').util.preconditions;
+var PrivateKey = require('bitcore').PrivateKey;
+var Script = require('bitcore').Script;
+var Address = require('bitcore').Address;
+var Networks = require('bitcore').Networks;
+var _ = require('bitcore').deps._;
+
+/**
+ * @constructor
+ */
+function Provider(opts) {
+  this.network = Networks.get(opts.network) || Networks.defaultNetwork;
+  if (!opts.paymentAddress) {
+    this.paymentKey = new PrivateKey();
+    this.paymentAddress = this.paymentKey.toAddress(this.network);
+  } else {
+    this.paymentAddress = new Address(opts.paymentAddress);
+  }
+
+  this.currentAmount = opts.currentAmount || 0;
+  this.key = opts.key || new PrivateKey();
+}
+
+Provider.prototype.getPublicKey = function getPublicKey() {
+  return this.key.publicKey;
+};
+
+Provider.prototype.signRefund = function signRefund(receivedData) {
+  var refund = new Refund(receivedData);
+  refund.sign(this.key);
+  this.refund = refund;
+  return refund;
+};
+
+Provider.prototype.validPayment = function validPayment(receivedData) {
+  var payment = new Payment(receivedData);
+  var newAmount;
+  var self = this;
+
+  payment.sign(this.key);
+  payment.outputs.map(function(output) {
+    if (output.script.toAddress(self.network).toString() === self.paymentAddress.toString()) {
+      newAmount = output.satoshis;
+    }
+  });
+  $.checkState(Script.Interpreter().verify(
+    payment.inputs[0].script,
+    payment.inputs[0].output.script
+  ), 'Script did not evaluate correctly (probably a bad signature received)');
+  $.checkState(!_.isUndefined(newAmount) && newAmount > this.currentAmount,
+               'A payment for a greater amount was already received');
+  this.paymentTx = payment;
+  this.currentAmount = newAmount;
+  return payment;
+};
+
+Provider.prototype.getPaymentTx = function getPaymentTx() {
+  return this.paymentTx.build();
+};
+
+module.exports = Provider;
+
+},{"./transactions/payment":5,"./transactions/refund":6,"bitcore":"bitcore"}],4:[function(require,module,exports){
+'use strict';
+
+var inherits = require('inherits');
+
+var $ = require('bitcore').util.preconditions;
+
+var PrivateKey = require('bitcore').PrivateKey;
+var Address = require('bitcore').Address;
+var Script = require('bitcore').Script;
+var Transaction = require('bitcore').Transaction;
+var _ = require('bitcore').deps._;
+
+
+/**
+ * A commitment transaction (also referred to as Lock transaction).
+ *
+ * @constructor
+ * @param {Object} opts
+ * @param {Array.<string>} opts.publicKeys
+ * @param {string|bitcore.Network} opts.network - livenet by default
+ */
+function Commitment(opts) {
+  $.checkArgument(opts.publicKeys && opts.publicKeys.length === 2, 'Must provide exactly two public keys');
+  Transaction.call(this, opts.transaction);
+
+  this.network = opts.network || 'livenet';
+  this.publicKeys = opts.publicKeys;
+  this.outscript = Script.buildMultisigOut(this.publicKeys, 2);
+  this.address = Address(this.outscript, this.network);
+  this.change(this.address);
+
+  this.keys = opts.keys ? _.map(opts.keys, function(privateKey) { return new PrivateKey(privateKey); }) : [];
+  Object.defineProperty(this, 'amount', {
+    configurable: false,
+    get: function() {
+      return this._inputAmount;
+    }
+  });
+  this._updateChange = undefined;
+}
+inherits(Commitment, Transaction);
+
+Commitment.prototype.toObject = function() {
+  var transaction = Transaction.prototype.toObject.apply(this);
+  return {
+    transaction: transaction,
+    publicKeys: _.map(this.publicKeys, function(publicKey) { return publicKey.toString(); }),
+    network: this.network.toString(),
+    keys: _.map(this.keys, function(privateKey) { return privateKey.toString(); })
+  };
+};
+
+/**
+ * @return {bitcore.Address}
+ */
+Commitment.prototype.getAddress = function() {
+  return this.address;
+};
+
+Commitment.prototype.addKey = function(key) {
+  this.keys.push(key);
+};
+
+module.exports = Commitment;
+
+},{"bitcore":"bitcore","inherits":7}],5:[function(require,module,exports){
+'use strict';
+
+var inherits = require('inherits');
+
+var Address = require('bitcore').Address;
+var Transaction = require('bitcore').Transaction;
+var PublicKey = require('bitcore').Publickey;
+var $ = require('bitcore').util.preconditions;
+var _ = require('bitcore').deps._;
+
+var FEE_AMOUNT = 10000;
+
+
+/**
+ * @constructor
+ * @param {Object} opts
+ * @param {bitcore.PublicKey} opts.publicKeys
+ * @param {bitcore.Address} opts.paymentAddress
+ * @param {bitcore.Address} opts.changeAddress
+ */
+function Payment(opts) {
+  if (!(this instanceof Payment)) {
+    return new Payment(opts);
+  }
+  Transaction.call(this, opts.transaction);
+
+  this.paymentAddress = new Address(opts.paymentAddress);
+  this._change = new Address(opts.changeAddress);
+
+  this.multisigOut = new Transaction.UnspentOutput(opts.multisigOut);
+  this.publicKeys = _.map(opts.publicKeys, PublicKey);
+  if (!this.inputs.length) {
+    this.from(this.multisigOut, this.publicKeys, 2);
+  }
+  if (this.outputs.length < 2) {
+    this._updateChangeOutput();
+  }
+  this.amount = this._outputAmount;
+  this.sequence = opts.sequence || 0;
+  this._fee = FEE_AMOUNT;
+  this.paid = opts.paid || 0;
+  $.checkArgument(_.isNumber(this.amount), 'Amount must be a number');
+  this._changeSetup = undefined;
+}
+inherits(Payment, Transaction);
+
+Payment.prototype._updateTransaction = function() {
+  this.outputs = [];
+  this._changeOutput = undefined;
+  this._outputAmount = 0;
+  this.to(this.paymentAddress, this.paid);
+  this.inputs[0].sequence = this.sequence;
+  this._changeSetup = false;
+};
+
+Payment.prototype.updateValue = function(delta) {
+  this.paid += delta;
+  this.sequence += 1;
+  this._updateTransaction();
+  return this;
+};
+
+Payment.prototype.toObject = function() {
+  return {
+    publicKeys: _.map(this.publicKeys, function(publicKey) { return publicKey.toString(); }),
+    multisigOut: this.multisigOut.toObject(),
+    amount: this.amount,
+    sequence: this.sequence,
+    paymentAddress: this.paymentAddress.toString(),
+    changeAddress: this._change.toString(),
+    transaction: Transaction.prototype.toObject.apply(this)
+  };
+};
+
+Payment.fromObject = function(obj) {
+  return new Payment(obj);
+};
+
+module.exports = Payment;
+
+},{"bitcore":"bitcore","inherits":7}],6:[function(require,module,exports){
+'use strict';
+
+var inherits = require('inherits');
+
+var Transaction = require('bitcore').Transaction;
+
+/**
+ * @constructor
+ */
+function Refund() {
+  Transaction.apply(this, arguments);
+  this._changeSetup = undefined;
+}
+inherits(Refund, Transaction);
+
+module.exports = Refund;
+
+},{"bitcore":"bitcore","inherits":7}],7:[function(require,module,exports){
+if (typeof Object.create === 'function') {
+  // implementation from standard node.js 'util' module
+  module.exports = function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor
+    ctor.prototype = Object.create(superCtor.prototype, {
+      constructor: {
+        value: ctor,
+        enumerable: false,
+        writable: true,
+        configurable: true
+      }
+    });
+  };
+} else {
+  // old school shim for old browsers
+  module.exports = function inherits(ctor, superCtor) {
+    ctor.super_ = superCtor
+    var TempCtor = function () {}
+    TempCtor.prototype = superCtor.prototype
+    ctor.prototype = new TempCtor()
+    ctor.prototype.constructor = ctor
+  }
+}
+
+},{}],"bitcore-channel":[function(require,module,exports){
+// Setup errors
+require('./lib/errors');
+
+module.exports = {
+  Consumer: require('./lib/consumer'),
+  Provider: require('./lib/provider'),
+  Transactions: {
+    Commitment: require('./lib/transactions/commitment'),
+    Refund: require('./lib/transactions/refund'),
+    Payment: require('./lib/transactions/payment')
+  }
+};
+
+},{"./lib/consumer":1,"./lib/errors":2,"./lib/provider":3,"./lib/transactions/commitment":4,"./lib/transactions/payment":5,"./lib/transactions/refund":6}]},{},[]);
