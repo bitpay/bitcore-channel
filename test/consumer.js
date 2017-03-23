6402eba42d35082b70e7d60b0097ee692002c25d
Chris Kleeschulte <chrisk@bitpay.com>
2017-03-23 15:58:49 -0400

    wip

--- test/consumer.js
+++ test/consumer.js
@@ -16,23 +16,30 @@ describe('Consumer', function() {
   var lockTime = Math.round(new Date('2020-02-29Z').getTime()/1000);
   var redeemScript = Script.buildCLTVRedeemScript([providerPrivKey.publicKey.toString(), consumerPrivKey.publicKey.toString()], lockTime);
   var prevTx = new Transaction(require('./testdata/previousTx.json').rawtx);
+  var flags = Interpreter.SCRIPT_VERIFY_P2SH
+  | Interpreter.SCRIPT_VERIFY_STRICTENC
+  | Interpreter.SCRIPT_VERIFY_DERSIG
+  | Interpreter.SCRIPT_VERIFY_LOW_S
+  | Interpreter.SCRIPT_VERIFY_MINIMALDATA
+  | Interpreter.SCRIPT_VERIFY_SIGPUSHONLY
+  | Interpreter.SCRIPT_VERIFY_CHECKLOCKTIMEVERIFY;
 
-  describe('commitment tx', function() {
+  var commitOpts = {
+    prevTx: prevTx,
+    prevTxOutputIndex: 0,
+    network: 'testnet',
+    satoshis: 300000000,
+    privateKey:  new PrivateKey('cQgpdm9P92YQSkvWZLATUVj4ADpgRkru8HyXDY92hPDVbFehx1bC', 'testnet'),
+    changeAddress: consumerPrivKey.toAddress().toString(),
+    redeemScript: redeemScript,
+    fee: 100000
+  };
 
-    var opts = {
-      prevTx: prevTx,
-      prevTxOutputIndex: 0,
-      network: 'testnet',
-      satoshis: 300000000,
-      privateKey:  new PrivateKey('cQgpdm9P92YQSkvWZLATUVj4ADpgRkru8HyXDY92hPDVbFehx1bC', 'testnet'),
-      changeAddress: consumerPrivKey.toAddress().toString(),
-      redeemScript: redeemScript,
-      fee: 100000
-    };
+  describe('Commitment Transaction', function() {
 
 
     it('should create a valid commitment tx', function() {
-      var commitmentTx = Consumer.createCommitmentTransaction(opts);
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
       commitmentTx.inputs[0].output = prevTx.outputs[0];
       commitmentTx.isFullySigned().should.be.true;
       commitmentTx.verify().should.be.true;
@@ -40,43 +47,163 @@ describe('Consumer', function() {
 
 
     it('should create a commitment tx that cannot be spent before the time lock', function() {
-      var commitmentTx = Consumer.createCommitmentTransaction(opts);
-      opts.privateKey = consumerPrivKey;
-      opts.commitmentTransaction = commitmentTx;
-      opts.toAddress = providerPrivKey.toAddress().toString();
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        commitmentTransaction: commitmentTx,
+        toAddress: providerPrivKey.toAddress().toString(),
+        changeAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000
+      };
       var spendingCommitmentTx = Consumer.createChannelTransaction(opts);
-      commitmentTx.sign(consumerPrivKey);
       var scriptSig = spendingCommitmentTx.inputs[0].script;
       var scriptPubKey = commitmentTx.outputs[0].script;
       var interpreter = new Interpreter();
-      interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0,
-        Interpreter.SCRIPT_VERIFY_P2SH
-        | Interpreter.SCRIPT_VERIFY_STRICTENC
-        | Interpreter.SCRIPT_VERIFY_MINIMALDATA
-        | Interpreter.SCRIPT_VERIFY_SIGPUSHONLY
-        | Interpreter.SCRIPT_CHECKLOCKTIMEVERIFY).should.be.false;
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.false
+      interpreter.errstr.should.equal('SCRIPT_ERR_UNSATISFIED_LOCKTIME');
     });
 
-    it('should allow spending of commitment tx after the lock time expires', function() {
-      var commitmentTx = Consumer.createCommitmentTransaction(opts);
-      var lockTime = Math.round((new Date().getTime()/1000)-30); //30 seconds ago
-      var redeemScript = Script.buildCLTVRedeemScript([providerPrivKey.publicKey.toString(), consumerPrivKey.publicKey.toString()], lockTime);
+    it('should allow spending of commitment tx after the lock time expires using default time lock setting', function() {
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        providerPrivateKey: null,
+        commitmentTransaction: commitmentTx,
+        toAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000
+      };
 
-      opts.redeemScript = redeemScript;
-
-      var spendingCommitmentTx = Consumer.createChannelTransaction(opts);
-      commitmentTx.sign(consumerPrivKey);
+      var spendingCommitmentTx = Consumer.createCommitmentRefundTransaction(opts);
       var scriptSig = spendingCommitmentTx.inputs[0].script;
       var scriptPubKey = commitmentTx.outputs[0].script;
       var interpreter = new Interpreter();
-      interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0,
-        Interpreter.SCRIPT_VERIFY_P2SH
-        | Interpreter.SCRIPT_VERIFY_STRICTENC
-        | Interpreter.SCRIPT_VERIFY_MINIMALDATA
-        | Interpreter.SCRIPT_VERIFY_SIGPUSHONLY
-        | Interpreter.SCRIPT_CHECKLOCKTIMEVERIFY).should.be.true;
-
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.true;
     });
+
+    it('should allow spending of commitment tx after the lock time expires using default time lock setting', function() {
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        providerPrivateKey: null,
+        commitmentTransaction: commitmentTx,
+        toAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000,
+        lockTime: (lockTime + 86400) //1 more day than the operand to CHECKLOCKTIMEVERIFY
+      };
+
+      var spendingCommitmentTx = Consumer.createCommitmentRefundTransaction(opts);
+      var scriptSig = spendingCommitmentTx.inputs[0].script;
+      var scriptPubKey = commitmentTx.outputs[0].script;
+      var interpreter = new Interpreter();
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.true;
+    });
+
+    it('should not allow spending of commitment tx if manually setting time lock setting', function() {
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        providerPrivateKey: null,
+        commitmentTransaction: commitmentTx,
+        toAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000,
+        lockTime: (lockTime - 86400) //1 more day than the operand to CHECKLOCKTIMEVERIFY
+      };
+
+      var spendingCommitmentTx = Consumer.createCommitmentRefundTransaction(opts);
+      var scriptSig = spendingCommitmentTx.inputs[0].script;
+      var scriptPubKey = commitmentTx.outputs[0].script;
+      var interpreter = new Interpreter();
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.false
+      interpreter.errstr.should.equal('SCRIPT_ERR_UNSATISFIED_LOCKTIME');
+    });
+
+    it('should not allow spending of commitment tx if spending tx input is finalized', function() {
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        providerPrivateKey: null,
+        commitmentTransaction: commitmentTx,
+        toAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000,
+        sequenceNumber: 0xffffffff
+      };
+
+      var spendingCommitmentTx = Consumer.createCommitmentRefundTransaction(opts);
+      var scriptSig = spendingCommitmentTx.inputs[0].script;
+      var scriptPubKey = commitmentTx.outputs[0].script;
+      var interpreter = new Interpreter();
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.false
+      interpreter.errstr.should.equal('SCRIPT_ERR_UNSATISFIED_LOCKTIME');
+    });
+
+
+  });
+
+  describe('Channel Transaction', function() {
+
+
+    it('should create a valid channel tx', function() {
+      var commitmentTx = Consumer.createCommitmentTransaction(commitOpts);
+      var opts = {
+        prevTx: prevTx,
+        prevTxOutputIndex: 0,
+        network: 'testnet',
+        satoshis: 300000000,
+        consumerPrivateKey: consumerPrivKey,
+        providerPrivateKey: null,
+        commitmentTransaction: commitmentTx,
+        toAddress: consumerPrivKey.toAddress().toString(),
+        redeemScript: redeemScript,
+        fee: 100000,
+        sequenceNumber: 0xffffffff
+      };
+
+      var spendingCommitmentTx = Consumer.createChannelTransaction(opts);
+      var scriptSig = spendingCommitmentTx.inputs[0].script;
+      var scriptPubKey = commitmentTx.outputs[0].script;
+      var interpreter = new Interpreter();
+      var res = interpreter.verify(scriptSig, scriptPubKey, spendingCommitmentTx, 0, flags);
+      res.should.be.false;
+      //this also means that the providers key was not supplied and therefore did not sign
+      interpreter.errstr.should.equal('SCRIPT_ERR_UNSATISFIED_LOCKTIME');
+      //this ensures that this transaction is a true 2 of 2, protecting both consumer and provider
+      spendingCommitmentTx.inputs[0].sequenceNumber.should.equal(0xffffffff);
+      spendingCommitmentTx.nLockTime.should.equal(0);
+      var sh = bitcore.Transaction.sighash;
+      var sig = bitcore.crypto.Signature.fromTxFormat(spendingCommitmentTx.inputs[0].script.chunks[1].buf);
+      sh.verify(spendingCommitmentTx, sig, consumerPrivKey.publicKey, 0, scriptPubKey);
+    });
+
   });
 });
 
